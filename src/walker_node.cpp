11b8ae217e360fce8a955e0b09610a6fd0a0a88a
Lbarret <lbarret94@gmail.com>
2020-11-20 22:25:15 -0500

    initial commit

---
+++ src/walker_node.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file walker_node.cpp
+ * @version 3.0
+ * @brief Ros broadcaster node that broadcasts a tf frame 
+ * @Created on: Nov 8, 2020
+ * @copyright 2020 
+ * @Author Loic Barret
+ */
+
+#include <sstream>
+#include <iostream>
+#include "ros/ros.h"
+#include "std_msgs/String.h"
